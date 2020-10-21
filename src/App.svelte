aecfc4f5dcac69edd81178850525f4afdfa1060a
nemo-omen <nemo.omen@gmail.com>
2020-10-21 08:03:23 -0500

    Fetch works

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,53 @@
 <script>
 	import { onMount } from 'svelte';
-
+	import {fade} from 'svelte/transition';
 	import { socket } from './socket.js';
+	
 	export let name;
 
 	$: graphics = [];
+
+	let dummyGraphic = {
+		"type": "lowerthird",
+        "show": "cvhp",
+        "scene": "A03",
+        "entranceAnimation": {
+            "type": "fly",
+            "direction": "left",
+            "duration": "500",
+            "delay": "0"
+        },
+        "exitAnimation": {
+            "type": "fly",
+            "direction": "left",
+            "duration": "500",
+            "delay": "0"
+        },
+        "playoutOptions": {
+            "loop": "true",
+            "onScreen": "7000",
+            "offScreen": "120000"
+        },
+        "subjects": [
+            {
+                "title": "David Wagner",
+                "subtitle": "KLST/KSAN"
+            },
+            {
+                "title": "Cody Brown",
+                "subtitle": "KLST/KSAN"
+            }
+        ],
+	}
 
 	socket.on('graphics', (data) => {
 		console.log('graphics event data: ', data);
 		updateGraphics();
 	});
+
+	socket.on('graphicsUpdates', (data) => {
+		console.log('graphicsUpdate: ', data);
+	})
 
 	async function updateGraphics() {
 		socket.get('/graphics', (body, response) => {
@@ -19,6 +57,24 @@
 		});
 	}
 
+	async function saveGraphic() {
+		const response = await fetch('http://localhost:1337/graphics', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(dummyGraphic),
+		});
+		const data = await response.json();
+	}
+
+	async function deleteGraphic(id) {
+		console.log(id);
+		const response = await fetch(`http://localhost:1337/graphics/${id}`, {
+			method: 'DELETE'
+		});
+		const data = await response.json();
+	}
 
 	onMount(async() => {
 		updateGraphics();
@@ -26,15 +82,13 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
 	<div class="vessel">
-	<img src="submarine.svg" alt="A Yellow Underwater Vessel" title="Vessel">
+		<img src="submarine.svg" alt="A Yellow Underwater Vessel" title="Vessel">
 	</div>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<p class="and">and</p>
-	<p>Visit the <a href="https://sailsjs.com/whats-that">Sails site</a> to learn how to build Sails apps.</p>
+	<h1>Hello {name}!</h1>
+	<button on:click={saveGraphic}>Add Another</button>
 	{#each graphics as graphic}
-		<h3>{graphic.type}</h3>
+		<h3 transition:fade={{duration:300}}>{graphic.type} <button on:click={() => deleteGraphic(graphic.id)}>Delete</button></h3>
 	{/each}
 </main>
 
@@ -54,15 +108,6 @@
 	}
 	.vessel {
 		margin: 7em 0;
-	}
-	.icon-credit {
-		margin-top: 10em;
-		font-size: 0.7rem;
-	}
-	.and {
-		font-weight: 100;
-		color: #0178ff;
-		font-size: 2em;
 	}
 	@media (min-width: 640px) {
 		main {
